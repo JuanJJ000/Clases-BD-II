db125afce678aea6a31ce34957e258c3b7ba3a39
JuanJJ000 <90078173+JuanJJ000@users.noreply.github.com>
2023-07-13 19:30:35 -0600

    Add files via upload

---
+++ Recaudacion por mes .sql
@@ -0,0 +1,36 @@
+create view RecaudacionesSeguros_SA
+as
+select
+	'Seguros SA' as Empresa,
+	'Monto X Gastos de Hospitalizacion' as TipoServicio,
+	(select(sum(dh.Costo)) from DetalleHospitalizacion dh) as Recaudacion
+	union all
+select
+	'Seguros SA' as Empresa,
+	'Monto X Consultas' as TipoServicio,
+	(select(sum(cs.TotalGastosAdicionales+cs.GastoFijo)) from Consulta cs) as Recaudacion
+	union all
+select
+	'Serviplus' as Empresa,
+	'Monto X Polizas' as TipoServicio,
+	(select(sum(pl.Costo)) from Poliza pl) as Recaudacion
+	union all
+select
+	'Serviplus' as Empresa,
+	'Monto X Repuestos' as TipoServicio,
+	(select(sum(drp.Precio)) from Serviplus.dbo.Detalle_Repuesto drp) as Recaudacion
+	union all
+select
+	'Serviplus' as Empresa,
+	'Monto X Servicios de Mantenimiento' as TipoServicio,
+	(select(sum(dm.Precio)) from Serviplus.dbo.Detalle_Mantenimiento dm) as Recaudacion
+
+create login Gerente with password='123456'
+create user persona from login Gerente
+grant select to persona
+
+
+db.Personas.find({peso: {$lte: 80, $lte: 100}}).pretty()
+
+
+use Seguros_SA
